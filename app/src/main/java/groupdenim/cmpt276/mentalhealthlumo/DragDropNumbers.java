76df6b609f2fa4ade27be5f90a75f6a3168710cb
Donny Wu <enderpicture@gmail.com>
2018-09-15 20:57:53 -0700

    init branch

---
+++ app/src/main/java/groupdenim/cmpt276/mentalhealthlumo/DragDropNumbers.java
@@ -0,0 +1,13 @@
+package groupdenim.cmpt276.mentalhealthlumo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DragDropNumbers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drag_drop_numbers);
+    }
+}
