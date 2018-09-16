feb3e69e32e4fb9c7bd1e187fc3ca2a6a6da5f1c
Donny Wu <enderpicture@gmail.com>
2018-09-16 03:31:31 -0700

    removed intent

--- app/src/main/java/groupdenim/cmpt276/mentalhealthlumo/MainActivity.java
+++ app/src/main/java/groupdenim/cmpt276/mentalhealthlumo/MainActivity.java
@@ -10,8 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent i = new Intent(this, DragDropNumbers.class);
-        startActivity(i);
     }
 }
